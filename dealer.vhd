091beb972560087e9fca73d6329e04138e3a92e9
amartora <alexmartorano@gmail.com>
2020-12-03 13:32:17 -0500

    added comment

--- dealer.vhd
+++ dealer.vhd
@@ -4,14 +4,14 @@ use ieee.numeric_std.all;
 
 entity dealer is
 port (
-        clock : in std_logic;
-        cardA : [however many bits we will use];
-        cardB : [however many bits we will use];
-        reset : in std_logic;
-        newcard : in std_logic;
-        available : in std_logic;
-        won : out std_logic;
-        lost : out std_logic;
-        hit : out std_logic;
-        stand : out std_logic);
-end dealer;
+		clock : in std_logic;
+		cardA : in std_logic_vector(0 to 8);
+		cardB :	in std_logic_vector(0 to 8);
+		reset : in std_logic;
+		newcard : in std_logic;
+		available : in std_logic;
+		won : out std_logic;
+		lost : out std_logic;
+		hit : out std_logic;
+		stand : out std_logic);
+end dealer;	
