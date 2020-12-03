c1e9d0803a49a18a3c03825f225eb2212d457cd5
Daniel Zatko <dzatko@stevens.edu>
2020-12-03 13:25:48 -0500

    playerchoice

--- playerchoice.vhd
+++ playerchoice.vhd
@@ -6,5 +6,12 @@ entity playerchoice is
 port (
         buttonA : in std_logic;
         buttonB : in std_logic;
-        choice : out std_logic);
+        choice : out std_logic_vector(0 to 2));
 end playerchoice;
+
+
+architecture choicearch of playerchoice is
+begin
+        choice <= '0' when (buttonA = '0');
+        choice <= '1' when (buttonB = '0') else '2';
+end choicearch 
