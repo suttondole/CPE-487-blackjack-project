9216ce4cddd2c56b7b1475dbaf64d55861cdd589
amartora <alexmartorano@gmail.com>
2020-12-03 13:35:33 -0500

    added comment

--- dealer.vhd
+++ dealer.vhd
@@ -2,7 +2,7 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
-entity dealer is
+entity dealer is --this needs another entity to deal with the outputs from this entity and take action
 port (
 		clock : in std_logic;
 		cardA : in std_logic_vector(0 to 8);
